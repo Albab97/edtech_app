632f3ea8d8791b41df8b7ff20f00f4b40b6e6da2
Albab97 <albabahmed21@yahoo.com>
2022-12-13 19:47:12 +0530

    sideBar added

---
+++ app.js
@@ -0,0 +1,10 @@
+var loginBtn = document.getElementById('login');
+var closeBtn = document.getElementById('close');
+var sideBar = document.getElementById('sideBar');
+
+function openSideBar(){
+    sideBar.style.display = "block";  
+}
+function closeSideBar(){
+    sideBar.style.display = "none"; 
+}
